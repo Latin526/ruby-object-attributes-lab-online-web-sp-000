3795170183393fd3b6699cb36c7449d6b441cece
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-04 17:16:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,23 +23,3 @@ lassie.breed
 puts lassie.name
 puts lassie.breed
 
-class Person
-  def name=(person_name)
-    @name = person_name
-  end
-  def name
-    @name
-  end
-  def job=(person_job)
-    @job = person_job
-  end
-  def job
-    @job
-  end
-end
-    Yasmin = Person.new
-    Yasmin.name = "Yasmin"
-    Yasmin.job = "Software Developer"
-    
-    puts Yasmin.name
-    puts Yasmin.job
